7c87a93a5b4a1ef4e95584475ed6c94ee3bf153a
Oren <60857194+nospecs@users.noreply.github.com>
2024-11-25 00:30:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='ovadare',  
-    version='0.1.0', 
-    author='Oren Mukades', 
-    author_email='ai@ovadare.com', 
-    description='A framework for conflict detection and resolution in multi-agent systems.',
-    long_description=open('README.md').read(), 
-    long_description_content_type='text/markdown',  # Format of README
-    url='https://github.com/nospecs/ovadare',  
-    packages=find_packages(),  
-    install_requires=[
-        # Dependencies from requirements.txt
-        "autogen>=0.2.0",
-        
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.7',
-)
+if __name__ == "__main__":
+    setup()
